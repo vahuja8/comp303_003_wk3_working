800c91b03ae91d80a079e1c54c0725679d39d3b7
vahuja8 <vahuja8@mycentennial.ca>
2024-09-19 10:45:54 -0400

    Demo for Section003

---
+++ Week3_FormExample/src/main/java/com/va/week3/springmvc/Greeting.java
@@ -0,0 +1,21 @@
+package com.va.week3.springmvc;
+
+public class Greeting {
+	
+	 private long id;
+	  private String content;
+	  
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
---
+++ Week3_FormExample/src/main/java/com/va/week3/springmvc/Week3FormExampleApplication.java
@@ -0,0 +1,13 @@
+package com.va.week3.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3FormExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3FormExampleApplication.class, args);
+	}
+
+}
